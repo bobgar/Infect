c88d7a3622ebe5a88dbda0539d14cec4333e2a0f
Kenneth Montgomery <kennethMontgo@gmail.com>
2018-01-27 15:43:07 -0500

    Add spawnTool to TestScene

---
+++ Assets/Scripts/SpawnToolEditor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SpawnTool))]
+class SpawnToolEditor : Editor
+{
+    private static bool m_editMode = false;
+    void OnSceneGUI()
+    {
+        Event e = Event.current;
+        if (m_editMode)
+        {
+            if (e.type == EventType.KeyUp && e.keyCode == KeyCode.Space)
+            {
+                // if()
+                // Ray worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+                // RaycastHit hitInfo;
+ 
+               
+                // if (Physics.Raycast(worldRay, out hitInfo))
+                // {
+                    GameObject circle = AssetDatabase.LoadAssetAtPath("Assets/Prefab/Circle.prefab", typeof(GameObject)) as GameObject;
+                    GameObject circleInstance = Instantiate(circle) as GameObject;
+                    Vector2 mousePos = Event.current.mousePosition;
+                    mousePos.y = Camera.current.pixelHeight - mousePos.y;
+                    // mousePos.x = Camera.current.pixelWidth - mousePos.x;
+                    Vector3 position = Camera.current.ScreenPointToRay(mousePos).origin;
+                    position = new Vector3(position.x, position.y, 0);
+                    circleInstance.transform.position = position;
+                    // Undo.RecordObject(circleInstance);
+                // }
+               Debug.Log("space pressed");
+            }
+ 
+        }
+
+    }
+    public override void OnInspectorGUI()
+    {
+        if (m_editMode)
+        {
+            if (GUILayout.Button("Disable Editing"))
+            {
+                m_editMode = false;
+            }
+        }
+        else
+        {
+            if (GUILayout.Button("Enable Editing"))
+            {
+                m_editMode = true;
+            }
+        }
+    }
+
+}
