5c40b79acfffe945f68cb972014c2c76ec6c09db
Kevin Kohler <kkohler@hitpointstudios.com>
2018-01-26 21:53:23 -0500

    First commit

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+
+    public Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        
+        Vector2 startingSpeed = new Vector2(Random.Range(-5f,5f),Random.Range(-5f,5f));
+        rigidBody.velocity = startingSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		    
+	}
+}
