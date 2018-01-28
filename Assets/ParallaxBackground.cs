e9897bad3b74f5b6f6f1f11527ae6f263d05c7d3
Kevin Kohler <kkohler@hitpointstudios.com>
2018-01-27 22:11:06 -0500

    Backgrounds now Parallax, I hope correclt

---
+++ Assets/ParallaxBackground.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour {
+
+    public SpriteRenderer s;
+    public Vector3 velocity;
+    private Vector3 _initPos;
+    
+	// Use this for initialization
+	void Start () {
+        _initPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += velocity;//new Vector3(.0025f, .0025f, 0f);
+
+        if (velocity.x > 0)
+        {
+            if (this.transform.position.x > _initPos.x + (s.bounds.size.x / 5f))
+            {
+                this.transform.position = _initPos;
+            }
+        }
+        else
+        {
+            if (this.transform.position.x < _initPos.x - (s.bounds.size.x / 5f))
+            {
+                this.transform.position = _initPos;
+            }
+        }
+
+    }
+}
